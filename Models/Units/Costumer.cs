f2b3a18061365ca3d66de5ccfc886da95a9424aa
Anastasiia <anastacia.vashuck@gmail.com>
2021-02-25 16:05:49 +0200

    *-*

--- Models/Units/Costumer.cs
+++ Models/Units/Costumer.cs
@@ -4,7 +4,7 @@ namespace Models
 {
     public class Customer : Person
     {
-        public List<Calendar> cart;
+        public List<Calendar> cart { get; set; }
 
         public Customer(string fn = "", string ln = "", List<Calendar> cart=null)
             : base(fn, ln)
